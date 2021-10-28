366bd8839b1443e3c61da784eb7af3bb7abf9024
andrews1022 <47801024+andrews1022@users.noreply.github.com>
2021-10-27 17:26:55 -0700

    removed some starter files, added global styles

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,93 @@
+import { createGlobalStyle } from 'styled-components';
+import theme from './theme';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 100%;
+
+    @media ${theme.mediaQueries.desktopSmall} {
+		  font-size: 87.5%;
+    }
+
+    @media ${theme.mediaQueries.tabletLandscape} {
+      font-size: 75%;
+    }
+  }
+
+  body {
+	  font-family: ${theme.fonts.openSans};
+    line-height: 1;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+    color: inherit;
+    font-size: inherit;
+    -webkit-font-smoothing: antialiased;
+    margin: 0;
+    padding: 0;
+    text-transform: lowercase;
+  }
+
+  button, 
+  input, 
+  textarea {
+    font-family: ${theme.fonts.openSans};
+  }
+
+  img,
+  svg {
+    border: 0;
+    display: block;
+    height: auto;
+    max-width: 100%;
+  }
+
+  a {
+    &:link,
+    &:visited {
+      text-decoration: none;
+    }
+
+    &:hover,
+    &:active {
+      outline: 0;
+    }
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  blockquote,
+  q {
+    quotes: none;
+  }
+
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  audio,
+  canvas,
+  video {
+    display: inline-block;
+    max-width: 100%;
+    zoom: 1;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.ts
@@ -0,0 +1,42 @@
+const theme = {
+	mediaQueries: {
+		desktopHD: 'only screen and (max-width: 1920px)',
+		desktopMedium: 'only screen and (max-width: 1680px)',
+		desktopSmall: 'only screen and (max-width: 1440px)',
+		laptop: 'only screen and (max-width: 1366px)',
+		laptopSmall: 'only screen and (max-width: 1280px)',
+		tabletLandscape: 'only screen and (max-width: 1024px)',
+		tabletMedium: 'only screen and (max-width: 900px)',
+		tabletPortrait: 'only screen and (max-width: 768px)',
+		mobileXLarge: 'only screen and (max-width: 640px)',
+		mobileLarge: 'only screen and (max-width: 576px)',
+		mobileMedium: 'only screen and (max-width: 480px)',
+		mobileSmall: 'only screen and (max-width: 415px)',
+		mobileXSmall: 'only screen and (max-width: 375px)',
+		mobileTiny: 'only screen and (max-width: 325px)'
+	},
+	shades: {
+		white: '#fff',
+		whiteSmoke: '#efefef',
+		black: '#000'
+	},
+	colors: {
+		lightCyan: '#cbfafa',
+		blueLagoon: '#046182',
+		charcoal: '#424242',
+		maire: '#2b2a24'
+	},
+	fonts: {
+		openSans: "'Open Sans', sans-serif"
+	},
+	fontWeights: {
+		light: 300,
+		regular: 400,
+		medium: 500,
+		semiBold: 600,
+		bold: 700,
+		extraBold: 800
+	}
+};
+
+export default theme;
